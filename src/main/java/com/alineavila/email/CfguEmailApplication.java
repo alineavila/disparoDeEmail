3b4880dd75bcc5b6c9a5f775493d9e06d4d56955
Aline Avila <aline.avila01@gmail.com>
2023-07-05 00:19:33 -0300

    iniciado o projeto com todas as dependências

---
+++ src/main/java/com/alineavila/email/CfguEmailApplication.java
@@ -0,0 +1,13 @@
+package com.alineavila.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfguEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfguEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alineavila/email/CfguEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alineavila.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfguEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
